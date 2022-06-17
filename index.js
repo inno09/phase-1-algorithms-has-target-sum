c86211f6d4826e4b2eb6ee64ca1c69db072487c7
innocent <innonger@gmail.com>
2022-06-17 18:00:05 +0300

    commit tree e5df73e49e77790368cb8335178eadd0e9521451

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    // n steps (depending on the length of the input array)
+    for  (let a = i+1; a <array.length; a++) {
+      let num = target - array[i]
+      // n * n steps (nested loop!)
+      if (num === array[a] ) {
+        return true
+      
+      }
+        
+      }
+       
+
+    }
+    return false;
+    // 1 step
 }
+  
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,11 +25,19 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
-
+*/ 
+  // for loop to go through the array
+  // for loop to find a matching number the sum ot which equals to the target
+  // return true if such a pair is found
+  // return false if none is found
 /*
   Add written explanation of your solution here
 */
+// define the function
+// loop through the array 
+// loop through the array while adding
+// return true if two elements in the array are equal to the target
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
